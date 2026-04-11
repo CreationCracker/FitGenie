e8d80145825c40598fa32264b22bb10bb5e5922e
Ujjwal-Guptaa296 <guptaujjwal934@gmail.com>
2026-04-12 02:39:35 +0530

    login and sign up cganges

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { loginUser, signupUser } from "../controller/userController.js"; 
+import { loginUser, signupUser, updateProfile, getUserProfile } from "../controller/userController.js";
 
+import {requireAuth} from "../middleware/auth.js"
 const router = Router();
 
 router.post('/login', loginUser);
 router.post('/signup', signupUser);
+router.put("/update-profile", requireAuth, updateProfile);
 
+
+router.get("/profile", requireAuth, getUserProfile);
 export default router;
